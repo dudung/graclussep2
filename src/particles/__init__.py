534a2c781c5ef685d29e1fd3f4ec1934e3fa37af
Sparisoma Viridi <dudung@gmail.com>
2024-07-24 21:32:05 +0700

    add new module: modifier

--- src/particles/__init__.py
+++ src/particles/__init__.py
@@ -1,5 +1,9 @@
 # particles/__init__.py
 
 from .generator import create_cluster_238_circular
+from .modifier import set_charge
 
-__all__ = ['create_cluster_238_circular']
+__all__ = [
+    'create_cluster_238_circular',
+    'set_charge'
+]
